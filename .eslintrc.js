b74d4aad1abd411fb7ee93449d483c41ca795af8
Cris Mantovani <crismantovani87@gmail.com>
2022-03-15 13:37:42 -0300

    feat: :sparkles: Instala ESLINT
    Instala o ESLINT no projeto utilizando o modelo do AIRBNB.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
